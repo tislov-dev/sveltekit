9eb2918be0d54ca9d7b62ba147d02723d376179b
tislov-dev <75918631+tislov-dev@users.noreply.github.com>
2022-06-18 00:41:17 +0000

    Initial Commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<!-- optional fallback -->
+<nav>
+  <ul>
+    <li><a href="./Home">Home</a></li>
+    <li><a href="./about">About</a></li>
+    <li><a href="./Contact">Contact</a></li>
+  </ul>
+</nav>
+<slot/>
+
+<style>
+  ul {
+    display:flex;
+    justify-content:space-evenly;
+    margin:auto;
+    padding:a 
+  }
+</style>
